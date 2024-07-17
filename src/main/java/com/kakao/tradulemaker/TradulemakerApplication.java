00e5344df7e2aeb9fd334c2a34f95e021f0a99c7
jaypaak <qkrdlswo98@gmail.com>
2024-07-17 18:40:07 +0900

    FIRST COMMIT

---
+++ src/main/java/com/kakao/tradulemaker/TradulemakerApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.tradulemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradulemakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradulemakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakao/tradulemaker/TradulemakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.tradulemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradulemakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
