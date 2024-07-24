215af6793d5ae714acde0ad70b7ebfe9bc4d14a2
jaypaak <qkrdlswo98@gmail.com>
2024-07-25 02:47:12 +0900

    REFACTORING - delete useless codes & refactor codes

--- src/main/java/com/kakao/tradulemaker/oauth/controller/OauthController.java
+++ src/main/java/com/kakao/tradulemaker/oauth/controller/OauthController.java
@@ -1,6 +1,5 @@
 package com.kakao.tradulemaker.oauth.controller;
 
-import com.kakao.tradulemaker.oauth.dto.res.ConnectKakaoDto;
 import com.kakao.tradulemaker.oauth.service.KakaoApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,28 +16,31 @@ public class OauthController {
 
   @Value("${kakao.base-uri.connect}")
   String uriForConnection;
+
   @Value("${kakao.base-uri.get-token}")
   String uriForToken;
-  @Value("${kakao.client-id}")
-  String clientId;
+
   @Value("${kakao.redirect-uri}")
   String redirectUri;
+
+  @Value("${kakao.client.id}")
+  String clientId;
+
+  @Value("${kakao.client.secret}")
+  String clientSecret;
 
   private final KakaoApi kakaoApi;
 
   @GetMapping("/connect")
-  public String connectKakao() {
-    ConnectKakaoDto connectKakaoDto = kakaoApi.getConnectKakaoDto(uriForConnection, clientId, redirectUri);
-    
-    return "redirect:" + connectKakaoDto.getRedirectUri();
+  public String connectOauth() {
+    String connectionUri = kakaoApi.getConnectionUri(uriForConnection, clientId, redirectUri);
+
+    return "redirect:" + connectionUri;
   }
 
   @GetMapping("/authorize")
-  public ResponseEntity<String> getCode(
-          @RequestParam String code
-  ) {
-    ResponseEntity<String> res = kakaoApi.getAccessToken(uriForToken, code, clientId, redirectUri);
+  public ResponseEntity<String> logIn(@RequestParam String code) {
 
-    return res;
+    return kakaoApi.getTokens(uriForToken, code, clientId, clientSecret, redirectUri);
   }
 }
--- src/main/java/com/kakao/tradulemaker/oauth/service/KakaoApi.java
+++ src/main/java/com/kakao/tradulemaker/oauth/service/KakaoApi.java
@@ -1,6 +1,5 @@
 package com.kakao.tradulemaker.oauth.service;
 
-import com.kakao.tradulemaker.oauth.dto.res.ConnectKakaoDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -15,59 +14,50 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class KakaoApi {
 
-  public ConnectKakaoDto getConnectKakaoDto(
+  public String getConnectionUri(
           String baseUri,
           String clientId,
           String redirectUri
   ) {
-    StringBuffer url =
-            new StringBuffer()
-                    .append(baseUri)
-                    .append("client_id=" + clientId)
-                    .append("&redirect_uri=" + redirectUri)
-                    .append("&response_type=code");
 
-    String agreementUri = url.toString();
-
-    System.out.println(agreementUri);
-
-    return new ConnectKakaoDto(agreementUri);
+    return baseUri +
+            "client_id=" + clientId +
+            "&redirect_uri=" + redirectUri +
+            "&response_type=code";
   }
 
-  public ResponseEntity<String> getAccessToken(
+  public ResponseEntity<String> getTokens(
           String baseUri,
           String code,
           String clientId,
+          String clientSecret,
           String redirectUri
   ) {
     HttpHeaders headers = new HttpHeaders();
     headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-
     MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
     body.add("grant_type", "authorization_code");
     body.add("client_id", clientId);
+    body.add("client_secret", clientSecret);
     body.add("redirect_uri", redirectUri);
     body.add("code", code);
 
     HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(body, headers);
 
-    return getResponse(baseUri, HttpMethod.POST, entity);
+    return fetchPost(baseUri, entity);
   }
 
-  private ResponseEntity<String> getResponse(
+  private ResponseEntity<String> fetchPost(
           String baseUri,
-          HttpMethod method,
           HttpEntity<MultiValueMap<String, String>> entity
   ) {
 
-    ResponseEntity<String> res = new RestTemplate().exchange(
+    return new RestTemplate().exchange(
             baseUri,
-            method,
+            HttpMethod.POST,
             entity,
             String.class
     );
-
-    return res;
   }
 }
