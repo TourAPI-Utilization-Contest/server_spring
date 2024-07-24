72e0f213cbddffc11b873c2f9214d1298e01aa70
jaypaak <qkrdlswo98@gmail.com>
2024-07-25 02:46:06 +0900

    DELETE - init redundant class

--- src/main/java/com/kakao/tradulemaker/oauth/dto/res/TestDto.java
+++ src/main/java/com/kakao/tradulemaker/oauth/dto/res/TestDto.java
@@ -1,13 +1,4 @@
 package com.kakao.tradulemaker.oauth.dto.res;
 
-import lombok.Getter;
-
-@Getter
-public class ConnectKakaoDto {
-
-  private final String redirectUri;
-
-  public ConnectKakaoDto(String redirectUri) {
-    this.redirectUri = redirectUri;
-  }
+public class TestDto {
 }
