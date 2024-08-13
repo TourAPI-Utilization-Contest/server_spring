5e778705f6adc8bee612d012fc33306d72a0de23
wingun322 <seunggun322@naver.com>
2024-08-13 20:01:06 +0900

    FIX - Fix refresh bug

--- src/main/java/com/kakao/tradulemaker/common/config/WebMvcConfig.java
+++ src/main/java/com/kakao/tradulemaker/common/config/WebMvcConfig.java
@@ -18,6 +18,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(interceptor)
             .excludePathPatterns("/api/oauth/connect")
-            .excludePathPatterns("/api/oauth/authenticate");
+            .excludePathPatterns("/api/oauth/authenticate**");
   }
 }
