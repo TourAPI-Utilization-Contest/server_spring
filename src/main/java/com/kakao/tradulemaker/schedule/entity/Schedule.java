0bc045c8e9d6a14f1dd2ac9273d305e02b278195
jaypaak <qkrdlswo98@gmail.com>
2024-08-14 19:54:34 +0900

    create schedule entity

--- src/main/java/com/kakao/tradulemaker/schedule/entity/Schedule.java
+++ src/main/java/com/kakao/tradulemaker/schedule/entity/Schedule.java
@@ -1,2 +1,33 @@
-package com.kakao.tradulemaker.schedule.entity;public class Schedule {
+package com.kakao.tradulemaker.schedule.entity;
+
+import com.kakao.tradulemaker.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Builder
+@Entity(name = "schedule")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "title")
+  private String title;
+
+  @Column(name = "starts_at")
+  private LocalDate startsAt;
+
+  @Column(name = "created_at")
+  private LocalDate createdAt;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
 }
