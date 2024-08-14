21a920f39eadc0dc16e206c87038c94c0352b07c
jaypaak <qkrdlswo98@gmail.com>
2024-08-14 19:54:03 +0900

    ADD - add schedule list in the Member entity

--- src/main/java/com/kakao/tradulemaker/member/entity/Member.java
+++ src/main/java/com/kakao/tradulemaker/member/entity/Member.java
@@ -1,8 +1,14 @@
 package com.kakao.tradulemaker.member.entity;
 
+import com.kakao.tradulemaker.schedule.entity.Schedule;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Builder
@@ -13,4 +19,7 @@ public class Member {
 
   @Id
   private Long id;
+
+  @OneToMany(mappedBy = "schedule", orphanRemoval = true, cascade = CascadeType.PERSIST)
+  private List<Schedule> schedules = new ArrayList<>();
 }
---
+++ src/main/java/com/kakao/tradulemaker/schedule/entity/Schedule.java
@@ -0,0 +1,2 @@
+package com.kakao.tradulemaker.schedule.entity;public class Schedule {
+}
