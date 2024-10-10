a2b5c906134cde73a587df873243f0d74e7d8486
wingun322 <seunggun322@naver.com>
2024-10-11 05:06:56 +0900

    ENHANCEMENT - add findByEmail

--- src/main/java/com/kakao/tradulemaker/member/repository/MemberRepository.java
+++ src/main/java/com/kakao/tradulemaker/member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.kakao.tradulemaker.member.repository;
 import com.kakao.tradulemaker.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Object> findByEmail(String email);
 }
